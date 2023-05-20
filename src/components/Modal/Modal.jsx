b122ae366468ea0ac62b0f131b0c9c2cbf624d11
AMIHbKA <95082331+AMIHbKA@users.noreply.github.com>
2023-05-20 14:28:50 +0300

    pause

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,16 +1,53 @@
-import { ModalStyled, Overlay } from './Modal.styled';
+import { Component } from 'react';
+import { CloseButton, ModalStyled, Overlay } from './Modal.styled';
 
-export const Modal = ({ onActive, children }) => {
-  const handleClick = event => {
+export class Modal extends Component {
+  state = {
+    isOpen: false,
+  };
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.onEscKey);
+    setTimeout(() => {
+      this.setState({ isOpen: true });
+    }, 250);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.onEscKey);
+  }
+
+  onEscKey = event => {
+    if (event.code !== 'Escape') {
+      return;
+    }
+
+    this.props.onActive();
+  };
+
+  handleClick = event => {
     const { target, currentTarget } = event;
     if (target === currentTarget) {
-      onActive();
+      this.props.onActive();
     }
   };
 
-  return (
-    <Overlay className="overlay" onClick={handleClick}>
-      <ModalStyled className="modal">{children}</ModalStyled>
-    </Overlay>
-  );
-};
+  render() {
+    const { isOpen } = this.state;
+    const modalClassName = isOpen ? 'modal-open' : '';
+    console.log(modalClassName);
+    return (
+      <Overlay onClick={this.handleClick}>
+        <ModalStyled className={modalClassName}>
+          {this.props.children}
+        </ModalStyled>
+        <CloseButton
+          width={48}
+          height={48}
+          type="button"
+          onClick={this.handleClick}
+        />
+      </Overlay>
+    );
+  }
+}
